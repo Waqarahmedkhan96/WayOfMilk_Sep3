774620c0834324b3d7443ec492ee91ec84e071dd
Piotr-Gala <355451@via.dk>
2025-12-07 20:45:58 +0100

    renaming test, so they match source code

--- Java/WoM_T3/src/test/java/sep3/service/DepartmentServiceImplTest.java
+++ Java/WoM_T3/src/test/java/sep3/service/DepartmentServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class DepartmentDataServiceTest {
+class DepartmentServiceImplTest {
 
     @Mock
     private DepartmentRepository mockDepartmentRepository;
