5eac556ddca98c3a7509bad19558ba05562b7fdb
Piotr-Gala <355451@via.dk>
2025-12-09 22:36:09 +0100

    working department :DD

--- Java/WoM_T3/src/main/java/sep3/entity/Department.java
+++ Java/WoM_T3/src/main/java/sep3/entity/Department.java
@@ -2,6 +2,9 @@ package sep3.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "departments") // FIX: Plural name for consistency with "users" table
 public class Department
@@ -13,6 +16,15 @@ public class Department
     @Enumerated(EnumType.STRING) // FIX: Stores "QUARANTINE" instead of "0" in the DB
     private DepartmentType type;
 
+    @OneToMany(mappedBy = "department")
+    private List<Cow> cows = new ArrayList<>();
+
+    @OneToMany(mappedBy = "fromDept")
+    private List<TransferRecord> transferRecordsFrom = new ArrayList<>();
+
+    @OneToMany(mappedBy = "toDept")
+    private List<TransferRecord> transferRecordsTo = new ArrayList<>();
+
     protected Department() {}
 
     public Department(DepartmentType type)
@@ -23,9 +35,9 @@ public class Department
     public long getId() { return id; }
     public void setId(long id) { this.id = id; }
     public DepartmentType getType() { return type; }
-    //public void setName(DepartmentType type) { this.type = type; }
-
-    // made a new from setName to setType for clarity
     public void setType(DepartmentType type) { this.type = type; }
 
+    public List<Cow> getCows() { return cows; }
+    public List<TransferRecord> getTransferRecordsFrom() { return transferRecordsFrom; }
+    public List<TransferRecord> getTransferRecordsTo() { return transferRecordsTo; }
 }
